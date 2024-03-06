12b77c094eaee639044bfb202624cbf6bc44f84b
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 12:29:18 +0900

    코드생성 타입스크립트로 컴포넌트를 작성하였습니다.

---
+++ Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Profile() {
+  return (
+    <View>
+      <Text>Name</Text>
+    </View>
+  );
+}
+
+export default Profile;
