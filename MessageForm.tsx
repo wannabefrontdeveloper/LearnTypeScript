b4b713cee34b5a8f3414d3557c8b548d8d1f7f5a
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 13:05:30 +0900

    코드수정 useRef를 사용하여 useRef에 담을 값의 타입을 Generic으로 설정해주었습니다.

--- MessageForm.tsx
+++ MessageForm.tsx
@@ -1,19 +1,23 @@
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 import {Button, Text, TextInput, View } from 'react-native';
 
 function MessageForm() {
   const [text,setText] = useState<string>('');
   const [lastMessage, setLastMessage] = useState<{
+    id: number;
     message: string,
     date: Date;
   } | null>(null);
+  const nextId = useRef<number>(1);
 
   const onPress = () => {
     setLastMessage({
       message: text,
       date: new Date(),
+      id: nextId.current,
     });
     setText('');
+    nextId.current += 1;
   }
   return (
     <View>
