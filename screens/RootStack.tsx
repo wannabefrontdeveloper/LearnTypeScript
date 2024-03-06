43226171d768912773765d82eed8870a4636b1b1
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 13:31:17 +0900

    코드생성 새로운 스텍 네비게이션을 생성해주었습니다.

---
+++ screens/RootStack.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate('Detail');
+  };
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="Open Detail" onPress={onPress} />
+    </View>
+  );
+}
+
+function DetailScreen() {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+}
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen component={HomeScreen} name="Home" />
+      <Stack.Screen component={DetailScreen} name="Detail" />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
