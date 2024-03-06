e6b86d1108db60c51d5844942bb66893c9fa34ef
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 12:39:51 +0900

    코드수정 children Props를 사용하도록 하였습니다.

--- Profile.tsx
+++ Profile.tsx
@@ -5,13 +5,15 @@ interface Props {
   name: string;
   isActive?: boolean;
   image:string;
+  children: React.ReactNode;
 }
 
-function Profile({name, isActive, image='https://picsum.photos/200'}: Props) {
+function Profile({name, isActive, image='https://picsum.photos/200', children,}: Props) {
   return (
     <View style={isActive && styles.activeStyle}>
       <Image source={{uri: image}} />
       <Text>{name}</Text>
+      <View>{children}</View>
     </View>
   );
 }
