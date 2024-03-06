6fef967b5d8df7b34ae6bc46821462be2b3ad43c
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 13:47:55 +0900

    코드수정 하단 탭 네비게이션에서 상위에 있는 RootStack의 DetailScreen에 접근하기위해 NavigationProp들을 합쳐주었습니다.

--- screens/MainTab.tsx
+++ screens/MainTab.tsx
@@ -4,12 +4,18 @@ import {
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
 import { Button, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { CompositeNavigationProp, NavigatorScreenParams, useNavigation } from '@react-navigation/native';
+import { RootStackNavigationProp } from './RootStack';
 
 type MainTabParamList = {
   Home: undefined;
   Account: undefined;
 };
-type MainTabNavigationProp = BottomTabNavigationProp<MainTabParamList>;
+
+export type MainTabNavigationprop = CompositeNavigationProp<RootStackNavigationProp, BottomTabNavigationProp<MainTabParamList>
+>;
+
+// 추후 RootStack 내부 화면에서 navigation.navigate('MainTab', {screen: 'Account'})가 가능하게 해줌
+export type MainTabNavigationScreenParams = NavigatorScreenParams<MainTabParamList>;
 
 const Tab = createBottomTabNavigator<MainTabParamList>();
