749a06aad7ca8a7c89ba8d934caede4f8ae21d19
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 12:53:55 +0900

    코드추가 lastMessage라는 상태를 만들어서 상태에 {message: string, date:Date} 또는 null 타입인 값을 담아주었습니다.

--- MessageForm.tsx
+++ MessageForm.tsx
@@ -1,11 +1,32 @@
 import React,{useState} from 'react';
-import { TextInput, View } from 'react-native';
+import {Button, Text, TextInput, View } from 'react-native';
 
 function MessageForm() {
   const [text,setText] = useState<string>('');
+  const [lastMessage, setLastMessage] = useState<{
+    message: string,
+    date: Date;
+  } | null>(null);
+
+  const onPress = () => {
+    setLastMessage({
+      message: text,
+      date: new Date(),
+    });
+    setText('');
+  }
   return (
     <View>
       <TextInput value={text} onChangeText={setText} />
+      <Button title="PRESS ME" onPress={onPress} />
+      {lastMessage && (
+        <View>
+          <Text>
+            마지막 메시지: {lastMessage.message} (
+              {lastMessage.date.toLocaleString()})
+          </Text>
+          </View>
+      )}
     </View>
   )
 }
