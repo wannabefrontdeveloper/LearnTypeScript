f2c6ff844eebdde7fb6ece0386d0a8716caabe8a
wannabefrontdeveloper <giwonk@naver.com>
2024-03-06 12:43:22 +0900

    코드수정 Profile 컴포넌트를 사용할때 children을 설정해주었습니다.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Text } from 'react-native';
 import Profile from './Profile';
 
 function App() {
-  return <Profile name="Jone Doe" />;
+  return( 
+  <Profile name="Jone Doe">
+    <Text>Hello World</Text>
+    </Profile>
+    );
 }
 
 export default App;
